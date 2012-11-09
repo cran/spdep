b6df154985d0000881af5c01507bb424d7636984
Roger Bivand <Roger.Bivand@nhh.no>
2012-11-09 12:18:39 +0000

    version 0.5-53

--- R/mcmcsamp.R
+++ R/mcmcsamp.R
@@ -1,19 +1,20 @@
 MCMCsamp <- function(object, mcmc = 1L, verbose = NULL, ...) UseMethod("MCMCsamp")
 # from lme4/R/AllGeneric.R
 MCMCsamp.spautolm <- function(object, mcmc = 1L, verbose = NULL, ...,
-    burnin=0L, thin=1L, seed=NA, verbose_step=100L, listw, control=list()) {
+    burnin=0L, scale=1, listw, control=list()) {
     con <- list(Imult=2, cheb_q=5, MC_p=16, MC_m=30, super=NULL,
         spamPivot="MMD", in_coef=0.1, type="MC",
         correct=TRUE, trunc=TRUE, SE_method="LU", nrho=200,
         interpn=2000, small_asy=TRUE, small=1500, SElndet=NULL,
         LU_order=FALSE)
+    timings <- list()
+    .ptime_start <- proc.time()
     nmsC <- names(con)
     con[(namc <- names(control))] <- control
     if (length(noNms <- namc[!namc %in% nmsC])) 
         warning("unknown names in control: ", paste(noNms, collapse = ", "))
     if (is.null(verbose)) verbose <- get("verbose", envir = .spdepOptions)
     stopifnot(is.logical(verbose))
-    if (verbose) stopifnot(verbose_step > 0)
     if (!inherits(listw, "listw")) 
         stop("No neighbourhood list")
     method <- object$method
@@ -50,27 +51,39 @@ MCMCsamp.spautolm <- function(object, mcmc = 1L, verbose = NULL, ...,
     Sweights <- as(as(Diagonal(x=weights), "symmetricMatrix"), 
         "CsparseMatrix")
     assign("Sweights", Sweights, envir=env)
+    timings[["data_setup"]] <- proc.time() - .ptime_start
+    .ptime_start <- proc.time()
 
     if (verbose) cat(paste("\nJacobian calculated using "))
 
     interval <- jacobianSetup(method, env, con, trs=object$trs,
         interval=object$interval)
     assign("interval", interval, envir=env)
+    timings[["logdet_setup"]] <- proc.time() - .ptime_start
+    .ptime_start <- proc.time()
 
     start <- c(object$lambda, object$fit$coefficients)
     V <- object$fdHess
     stopifnot(nrow(V) == ncol(V))
     stopifnot(nrow(V) == length(start))
-    res <- MCMCmetrop1R(fun=f_spautolm_hess, theta.init=start, burnin=burnin,
-        mcmc=mcmc, thin=thin, verbose=ifelse(verbose, verbose_step, 0),
-        seed=seed, logfun=TRUE, V=V, env=env)
+    res0 <- rwmetrop(logpost=f_spautolm_hess, start=start,
+        proposal=list(var=V, scale=scale), m=(mcmc+burnin), env=env)
+#        MCMCmetrop1R(fun=f_spautolm_hess, theta.init=start, burnin=burnin,
+#        mcmc=mcmc, thin=thin, verbose=ifelse(verbose, verbose_step, 0),
+#        seed=seed, logfun=TRUE, V=V, env=env)
+    res <- as.mcmc(res0$par[(burnin+1):(mcmc+burnin),])
+    attr(res, "accept") <- res0$accept
     colnames(res) <- c(names(object$lambda), names(object$fit$coefficients))
+    attr(res, "type") <- family
+    timings[["rwmetrop"]] <- proc.time() - .ptime_start
+    attr(res, "timings") <- do.call("rbind", timings)[, c(1, 3)]
     res
 }
 
 MCMCsamp.sarlm <- function(object, mcmc = 1L, verbose = NULL, ...,
-    burnin=0L, thin=1L, seed=NA, verbose_step=100L, listw, listw2=NULL,
-    control=list()) {
+    burnin=0L, scale=1, listw, listw2=NULL, control=list()) {
+    timings <- list()
+    .ptime_start <- proc.time()
     con <- list(compiled_sse=FALSE, Imult=2, cheb_q=5, MC_p=16, MC_m=30,
         super=NULL, spamPivot="MMD", in_coef=0.1, type="MC",
         correct=TRUE, trunc=TRUE, SE_method="LU", nrho=200,
@@ -82,7 +95,6 @@ MCMCsamp.sarlm <- function(object, mcmc = 1L, verbose = NULL, ...,
         warning("unknown names in control: ", paste(noNms, collapse = ", "))
     if (is.null(verbose)) verbose <- get("verbose", envir = .spdepOptions)
     stopifnot(is.logical(verbose))
-    if (verbose) stopifnot(verbose_step > 0)
     if (!inherits(listw, "listw")) 
         stop("No neighbourhood list")
     type <- object$type
@@ -114,6 +126,8 @@ MCMCsamp.sarlm <- function(object, mcmc = 1L, verbose = NULL, ...,
     if (is.numeric(object$resvar)) V <- object$resvar[-1,-1]
     else V <- object$fdHess
     stopifnot(nrow(V) == ncol(V))
+    timings[["data_setup"]] <- proc.time() - .ptime_start
+    .ptime_start <- proc.time()
 
     if (verbose) cat(paste("\nJacobian calculated using "))
 
@@ -144,6 +158,8 @@ MCMCsamp.sarlm <- function(object, mcmc = 1L, verbose = NULL, ...,
         assign("first_time", TRUE, envir=env)
 
     }
+    timings[["logdet_setup"]] <- proc.time() - .ptime_start
+    .ptime_start <- proc.time()
 
     if (Type == "LAG") {
         if (con$compiled_sse) {
@@ -152,14 +168,18 @@ MCMCsamp.sarlm <- function(object, mcmc = 1L, verbose = NULL, ...,
         }
         start <- c(object$rho, object$coefficients)
         stopifnot(nrow(V) == length(start))
-        res <- MCMCmetrop1R(fun=f_laglm_hess, theta.init=start,
-            burnin=burnin, mcmc=mcmc, thin=thin, verbose=ifelse(verbose,
-            verbose_step, 0), seed=seed, logfun=TRUE, V=V, env=env)
+        res0 <- rwmetrop(logpost=f_laglm_hess, start=start,
+            proposal=list(var=V, scale=scale), m=(mcmc+burnin), env=env)
+#        res <- MCMCmetrop1R(fun=f_laglm_hess, theta.init=start,
+#            burnin=burnin, mcmc=mcmc, thin=thin, verbose=ifelse(verbose,
+#            verbose_step, 0), seed=seed, logfun=TRUE, V=V, env=env)
 
         if (con$compiled_sse) {
             .Call("hess_lag_free", get("ptr", envir=env),
                  PACKAGE="spdep")
         }
+        res <- as.mcmc(res0$par[(burnin+1):(mcmc+burnin),])
+        attr(res, "accept") <- res0$accept
         colnames(res) <- c(names(object$rho), names(object$coefficients))
         attr(res, "type") <- object$type
 
@@ -174,14 +194,18 @@ MCMCsamp.sarlm <- function(object, mcmc = 1L, verbose = NULL, ...,
         }
         start <- c(object$lambda, object$coefficients)
         stopifnot(nrow(V) == length(start))
-        res <- MCMCmetrop1R(fun=f_errlm_hess, theta.init=start,
-            burnin=burnin, mcmc=mcmc, thin=thin, verbose=ifelse(verbose,
-            verbose_step, 0), seed=seed, logfun=TRUE, V=V, env=env)
+        res0 <- rwmetrop(logpost=f_errlm_hess, start=start,
+            proposal=list(var=V, scale=scale), m=(mcmc+burnin), env=env)
+#        res <- MCMCmetrop1R(fun=f_errlm_hess, theta.init=start,
+#            burnin=burnin, mcmc=mcmc, thin=thin, verbose=ifelse(verbose,
+#            verbose_step, 0), seed=seed, logfun=TRUE, V=V, env=env)
 
         if (con$compiled_sse) {
             .Call("hess_error_free", get("ptr", envir=env),
                 PACKAGE="spdep")
         }
+        res <- as.mcmc(res0$par[(burnin+1):(mcmc+burnin),])
+        attr(res, "accept") <- res0$accept
         colnames(res) <- c(names(object$lambda), names(object$coefficients))
         attr(res, "type") <- object$type
 
@@ -194,13 +218,19 @@ MCMCsamp.sarlm <- function(object, mcmc = 1L, verbose = NULL, ...,
         assign("w2wy", w2wy, envir=env)
         start <- c(object$rho, object$lambda, object$coefficients)
         stopifnot(nrow(V) == length(start))   
-        res <- MCMCmetrop1R(fun=f_sac_hess, theta.init=start,
-            burnin=burnin, mcmc=mcmc, thin=thin, verbose=ifelse(verbose,
-            verbose_step, 0), seed=seed, logfun=TRUE, V=V, env=env)
+        res0 <- rwmetrop(logpost=f_sac_hess, start=start,
+            proposal=list(var=V, scale=scale), m=(mcmc+burnin), env=env)
+#        res <- MCMCmetrop1R(fun=f_sac_hess, theta.init=start,
+#            burnin=burnin, mcmc=mcmc, thin=thin, verbose=ifelse(verbose,
+#            verbose_step, 0), seed=seed, logfun=TRUE, V=V, env=env)
+        res <- as.mcmc(res0$par[(burnin+1):(mcmc+burnin),])
+        attr(res, "accept") <- res0$accept
         colnames(res) <- c(names(object$rho), names(object$lambda),
             names(object$coefficients))
         attr(res, "type") <- object$type
     }
+    timings[["rwmetrop"]] <- proc.time() - .ptime_start
+    attr(res, "timings") <- do.call("rbind", timings)[, c(1, 3)]
     res
 }
 
